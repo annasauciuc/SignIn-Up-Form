7e7460c7be251d409245faf16837f2db9b73892e
annasauciuc <sauciuc.anamaria.dd@gmail.com>
2020-01-16 22:14:17 +0000

    change style for form sign-in

--- src/Overlay/Overlay.js
+++ src/Overlay/Overlay.js
@@ -4,7 +4,6 @@ import "./Overlay.css";
 
 class Overlay extends Component {
   render() {
-      console.log('this.props', this.props)
     const { signIn, signUp } = this.props;
     return (
       <section className="overlay-container">
@@ -12,7 +11,7 @@ class Overlay extends Component {
           <div className="overlay-panel overlay-left">
             <h1>Welcome Back!</h1>
             <p>
-              To keep connected with us please login with your personal info
+             Please login with your personal info
             </p>
             <button onClick={() => signIn()} className="ghost" id="signIn">
               Sign In
@@ -20,7 +19,7 @@ class Overlay extends Component {
           </div>
           <div className="overlay-panel overlay-right">
             <h1>Hello, Friend!</h1>
-            <p>Enter your personal details and start journey with us</p>
+            <p>Enter your personal details and start the journey with us</p>
             <button onClick={() => signUp()} className="ghost" id="signUp">
               Sign Up
             </button>
